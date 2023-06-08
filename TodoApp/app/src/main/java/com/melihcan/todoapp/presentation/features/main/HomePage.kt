12ef571be0101247664df81209b786ba26d570b1
Melihcan Sarıkaya <sarikayamelihcan-01@outlook.com>
2023-06-08 23:01:05 +0300

    main page add scaffold

--- TodoApp/app/src/main/java/com/melihcan/todoapp/presentation/features/main/HomePage.kt
+++ TodoApp/app/src/main/java/com/melihcan/todoapp/presentation/features/main/HomePage.kt
@@ -1,11 +1,23 @@
 package com.melihcan.todoapp.presentation.features.main
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomePage(
     navController: NavController
 ) {
+    Scaffold() {padding ->
+        Column(
+            modifier = Modifier.padding(padding)
+        ) {
 
+        }
+    }
 }
