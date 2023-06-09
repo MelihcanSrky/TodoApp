62cafe60f6953fe5f09181f9b24a74c60c24e0bb
Melihcan Sarıkaya <sarikayamelihcan-01@outlook.com>
2023-06-09 20:01:34 +0300

    main page add Text

--- TodoApp/app/src/main/java/com/melihcan/todoapp/presentation/features/main/HomePage.kt
+++ TodoApp/app/src/main/java/com/melihcan/todoapp/presentation/features/main/HomePage.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
@@ -17,7 +18,7 @@ fun HomePage(
         Column(
             modifier = Modifier.padding(padding)
         ) {
-
+            Text("Main Page")
         }
     }
 }
